15030320742df22cf01a8859dce8b5fe34c248dd
Jared Rummler <jared.rummler@gmail.com>
2018-01-17 15:50:55 -0800

    Initial commit

---
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jaredrummler.cyanea.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.jaredrummler.cyanea.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
