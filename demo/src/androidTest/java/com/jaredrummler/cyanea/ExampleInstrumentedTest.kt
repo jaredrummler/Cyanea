26dec8b716d245c4311f03a02d9605d87414dbd7
Jared Rummler <jared.rummler@gmail.com>
2018-01-17 16:12:50 -0800

    Update code style
    Now using Square's Android code style.
    https://github.com/square/java-code-styles

--- demo/src/androidTest/java/com/jaredrummler/cyanea/ExampleInstrumentedTest.kt
+++ demo/src/androidTest/java/com/jaredrummler/cyanea/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package com.jaredrummler.cyanea
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +13,10 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.jaredrummler.cyanea", appContext.packageName)
-    }
+  @Test
+  fun useAppContext() {
+    // Context of the app under test.
+    val appContext = InstrumentationRegistry.getTargetContext()
+    assertEquals("com.jaredrummler.cyanea", appContext.packageName)
+  }
 }
--- demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
@@ -2,12 +2,11 @@ package com.jaredrummler.cyanea.demo
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.jaredrummler.cyanea.R
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
 }
