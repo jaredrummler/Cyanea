34341307d443bdec04158307a18366556bba1f4d
Jared Rummler <jared.rummler@gmail.com>
2018-03-20 22:35:05 -0700

    Add a decorator to the sample.
    This is temporarily being used to test the custom LayoutInflator

--- demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
@@ -1,7 +1,14 @@
 package com.jaredrummler.cyanea.demo
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.AttributeSet
+import android.view.View
+import android.widget.TextView
+import com.jaredrummler.cyanea.inflator.CyaneaContextWrapper
+import com.jaredrummler.cyanea.inflator.decor.Decorator
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +16,19 @@ class MainActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
   }
+
+  override fun attachBaseContext(newBase: Context) {
+    super.attachBaseContext(CyaneaContextWrapper(newBase,
+        decorators = arrayOf(MyDecorator())))
+  }
+
+  class MyDecorator : Decorator {
+    override fun apply(view: View, attrs: AttributeSet) {
+      if (view is TextView) {
+        view.setTextColor(Color.parseColor("#0099cc"))
+      }
+    }
+
+  }
+
 }
