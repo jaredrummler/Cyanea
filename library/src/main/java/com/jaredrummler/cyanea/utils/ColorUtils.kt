ef95c18a5558c567cd13e6cfcdda7798e9aa1c82
Jared Rummler <jaredrummler@gmail.com>
2018-06-14 14:18:34 -0700

    Create method to parse color strings

--- library/src/main/java/com/jaredrummler/cyanea/utils/ColorUtils.kt
+++ library/src/main/java/com/jaredrummler/cyanea/utils/ColorUtils.kt
@@ -21,7 +21,8 @@ class ColorUtils private constructor() {
      * @return darker version of specified color.
      */
     @JvmStatic
-    @ColorInt fun darker(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
+    @ColorInt
+    fun darker(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
       return Color.argb(Color.alpha(color), Math.max((Color.red(color) * factor).toInt(), 0),
           Math.max((Color.green(color) * factor).toInt(), 0),
           Math.max((Color.blue(color) * factor).toInt(), 0))
@@ -35,7 +36,8 @@ class ColorUtils private constructor() {
      * @return lighter version of the specified color.
      */
     @JvmStatic
-    @ColorInt fun lighter(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
+    @ColorInt
+    fun lighter(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
       val alpha = Color.alpha(color)
       val red = ((Color.red(color) * (1 - factor) / 255 + factor) * 255).toInt()
       val green = ((Color.green(color) * (1 - factor) / 255 + factor) * 255).toInt()
@@ -97,8 +99,99 @@ class ColorUtils private constructor() {
      * @return The new color value
      */
     @JvmStatic
-    @ColorInt fun adjustAlpha(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
+    @ColorInt
+    fun adjustAlpha(@ColorInt color: Int, @FloatRange(from = 0.0, to = 1.0) factor: Float): Int {
       return (factor * 255.0f).toInt() shl 24 or (color and 0x00ffffff)
+    }
+
+    @JvmStatic
+    fun toHex(@ColorInt color: Int, alpha: Boolean = true): String {
+      return if (alpha) {
+        String.format("%08X", (color))
+      } else {
+        String.format("%06X", 0xFFFFFF and color)
+      }
+    }
+
+    /**
+     * Parse the color string, and return the corresponding color-int.
+     * If the string cannot be parsed, throws an IllegalArgumentException
+     * exception.
+     */
+    @JvmStatic
+    @ColorInt
+    fun parseColor(colorString: String): Int {
+      try {
+        if (colorString.startsWith("#")) {
+          return parseColor(colorString.substring(1))
+        }
+
+        val length = colorString.length
+        val a: Int
+        var r: Int
+        val g: Int
+        var b = 0
+
+        when {
+          length == 0 -> {
+            r = 0
+            a = 255
+            g = 0
+          }
+          length <= 2 -> {
+            a = 255
+            r = 0
+            b = Integer.parseInt(colorString, 16)
+            g = 0
+          }
+          length == 3 -> {
+            a = 255
+            r = Integer.parseInt(colorString.substring(0, 1), 16)
+            g = Integer.parseInt(colorString.substring(1, 2), 16)
+            b = Integer.parseInt(colorString.substring(2, 3), 16)
+          }
+          length == 4 -> {
+            a = 255
+            r = Integer.parseInt(colorString.substring(0, 2), 16)
+            g = r
+            r = 0
+            b = Integer.parseInt(colorString.substring(2, 4), 16)
+          }
+          length == 5 -> {
+            a = 255
+            r = Integer.parseInt(colorString.substring(0, 1), 16)
+            g = Integer.parseInt(colorString.substring(1, 3), 16)
+            b = Integer.parseInt(colorString.substring(3, 5), 16)
+          }
+          length == 6 -> {
+            a = 255
+            r = Integer.parseInt(colorString.substring(0, 2), 16)
+            g = Integer.parseInt(colorString.substring(2, 4), 16)
+            b = Integer.parseInt(colorString.substring(4, 6), 16)
+          }
+          length == 7 -> {
+            a = Integer.parseInt(colorString.substring(0, 1), 16)
+            r = Integer.parseInt(colorString.substring(1, 3), 16)
+            g = Integer.parseInt(colorString.substring(3, 5), 16)
+            b = Integer.parseInt(colorString.substring(5, 7), 16)
+          }
+          length == 8 -> {
+            a = Integer.parseInt(colorString.substring(0, 2), 16)
+            r = Integer.parseInt(colorString.substring(2, 4), 16)
+            g = Integer.parseInt(colorString.substring(4, 6), 16)
+            b = Integer.parseInt(colorString.substring(6, 8), 16)
+          }
+          else -> {
+            b = -1
+            g = -1
+            r = -1
+            a = -1
+          }
+        }
+        return Color.argb(a, r, g, b)
+      } catch (e: NumberFormatException) {
+        return Color.parseColor(colorString)
+      }
     }
 
   }
