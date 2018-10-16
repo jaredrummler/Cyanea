42931ba30e20b212561ce58464617fbed7b1c7f7
Jared Rummler <jared.rummler@gmail.com>
2018-10-15 23:33:00 -0700

    Use the activity icon res when creating the TaskDescription on API 28+

--- library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV21.kt
+++ library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV21.kt
@@ -3,7 +3,9 @@ package com.jaredrummler.cyanea.delegate
 import android.annotation.TargetApi
 import android.app.Activity
 import android.app.ActivityManager
+import android.content.ComponentName
 import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
 import android.graphics.Color
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
@@ -28,13 +30,29 @@ internal open class CyaneaDelegateImplV21(
 
   private fun setTaskDescription() {
     try {
-      val icon = activity.packageManager.getApplicationIcon(activity.packageName)
-      (icon as? BitmapDrawable)?.bitmap?.let { bitmap ->
-        val primary = cyanea.primary
-        // Strip out any alpha
-        val color = Color.rgb(Color.red(primary), Color.green(primary), Color.blue(primary))
-        val td = ActivityManager.TaskDescription(activity.title.toString(), bitmap, color)
+      val primary = cyanea.primary
+      val color = Color.rgb(Color.red(primary), Color.green(primary), Color.blue(primary)) // Strip out any alpha
+
+      val componentName = ComponentName(activity, activity::class.java)
+      val activityInfo = activity.packageManager.getActivityInfo(componentName, 0)
+
+      activityInfo?.iconResource.takeIf { it != 0 }?.let { iconRes ->
+        val td = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+          ActivityManager.TaskDescription(activity.title.toString(), iconRes, color)
+        } else {
+          val icon = BitmapFactory.decodeResource(activity.resources, iconRes) ?: return
+          @Suppress("DEPRECATION")
+          ActivityManager.TaskDescription(activity.title.toString(), icon, color)
+        }
         activity.setTaskDescription(td)
+        return
+      } ?: run {
+        val icon = activity.packageManager.getApplicationIcon(activity.packageName)
+        (icon as? BitmapDrawable)?.bitmap?.let { bitmap ->
+          @Suppress("DEPRECATION")
+          val td = ActivityManager.TaskDescription(activity.title.toString(), bitmap, color)
+          activity.setTaskDescription(td)
+        }
       }
     } catch (ignored: PackageManager.NameNotFoundException) {
     }
