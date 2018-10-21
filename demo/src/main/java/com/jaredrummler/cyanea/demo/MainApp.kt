64d798e11d154d988e36a9bbb37b2ccbc55046da
Jared Rummler <jared.rummler@gmail.com>
2018-10-21 16:49:33 -0700

    Update demo with BottomAppBar background color

--- demo/src/main/java/com/jaredrummler/cyanea/demo/MainApp.kt
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/MainApp.kt
@@ -1,16 +1,20 @@
 package com.jaredrummler.cyanea.demo
 
+import android.view.View
 import com.jaredrummler.cyanea.Cyanea
 import com.jaredrummler.cyanea.CyaneaApp
+import com.jaredrummler.cyanea.inflator.CyaneaViewProcessor
 import com.jaredrummler.cyanea.inflator.decor.CyaneaDecorator
 import com.jaredrummler.cyanea.inflator.decor.FontDecorator
 
-class MainApp : CyaneaApp(), CyaneaDecorator.Provider {
+class MainApp : CyaneaApp(), CyaneaDecorator.Provider, CyaneaViewProcessor.Provider {
 
   override fun onCreate() {
     super.onCreate()
     Cyanea.loggingEnabled = true
   }
+
+  override fun getViewProcessors(): Array<CyaneaViewProcessor<out View>> = arrayOf()
 
   override fun getDecorators(): Array<CyaneaDecorator> = arrayOf(FontDecorator())
 
