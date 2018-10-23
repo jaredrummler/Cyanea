88a711264f15e5dd2de2df644a9c8f3afd2277a5
Jared Rummler <jared.rummler@gmail.com>
2018-10-22 17:48:31 -0700

    Update demo with widgets tab

--- demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/MainActivity.kt
@@ -3,12 +3,23 @@ package com.jaredrummler.cyanea.demo
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentStatePagerAdapter
+import com.jaredrummler.cyanea.Cyanea
 import com.jaredrummler.cyanea.app.CyaneaAppCompatActivity
 import com.jaredrummler.cyanea.demo.fragments.AboutFragment
+import com.jaredrummler.cyanea.demo.fragments.WidgetsFragment
 import com.jaredrummler.cyanea.demo.themes.ThemePickerActivity
 import kotlinx.android.synthetic.main.activity_main.bar
 import kotlinx.android.synthetic.main.activity_main.fab
+import kotlinx.android.synthetic.main.activity_main.tabLayout
+import kotlinx.android.synthetic.main.activity_main.viewPager
 
 class MainActivity : CyaneaAppCompatActivity() {
 
@@ -16,14 +27,18 @@ class MainActivity : CyaneaAppCompatActivity() {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-          .add(R.id.container, AboutFragment())
-          .commit()
-    }
-
     fab.setOnClickListener {
       startActivity(Intent(this, ThemePickerActivity::class.java))
+    }
+
+    viewPager.adapter = MyPagerAdapter(this)
+    tabLayout.setupWithViewPager(viewPager)
+
+    if (tabLayout.tabCount > 0) {
+      val tab = (tabLayout.getChildAt(0) as ViewGroup).getChildAt(0)
+      val p = tab.layoutParams as ViewGroup.MarginLayoutParams
+      p.setMargins(resources.getDimension(R.dimen.default_margin).toInt(), 0, 0, 0)
+      tab.requestLayout()
     }
 
     bar.replaceMenu(R.menu.bottom_bar_menu)
@@ -43,5 +58,39 @@ class MainActivity : CyaneaAppCompatActivity() {
   }
 
   override fun getThemeResId(): Int = cyanea.themes.actionBarTheme
+
+}
+
+class MyPagerAdapter(private val activity: FragmentActivity)
+  : FragmentStatePagerAdapter(activity.supportFragmentManager) {
+
+  private val items = activity.resources.getStringArray(R.array.tabs)
+
+  override fun getItem(position: Int): Fragment {
+    return when (items[position]) {
+      activity.getString(R.string.tab_about) -> AboutFragment()
+      activity.getString(R.string.tab_widgets) -> WidgetsFragment()
+      activity.getString(R.string.tab_dialogs) -> DummyFragment()
+      else -> throw IllegalArgumentException("No fragment associated with tab '${items[position]}'")
+    }
+  }
+
+  override fun getPageTitle(position: Int): CharSequence? = items[position]
+
+  override fun getCount(): Int = items.size
+
+}
+
+class DummyFragment : Fragment() {
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return FrameLayout(requireActivity())
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    view.setBackgroundColor(Cyanea.instance.backgroundColor)
+  }
+
 
 }
--- demo/src/main/java/com/jaredrummler/cyanea/demo/fragments/WidgetsFragment.kt
+++ demo/src/main/java/com/jaredrummler/cyanea/demo/fragments/WidgetsFragment.kt
@@ -7,10 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.jaredrummler.cyanea.demo.R
 
-class ButtonsFragment : Fragment() {
+class WidgetsFragment : Fragment() {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-    return inflater.inflate(R.layout.fragment_buttons, container, false)
+    return inflater.inflate(R.layout.fragment_widgets, container, false)
   }
 
 }
