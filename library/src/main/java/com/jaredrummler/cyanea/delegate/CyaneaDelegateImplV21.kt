a61a93b72be337eeb2c5bc9e7da5b903d72100af
Jared Rummler <jared.rummler@gmail.com>
2018-11-13 22:20:12 -0800

    Preload accent color on Android Lollipop

--- library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV21.kt
+++ library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV21.kt
@@ -5,12 +5,17 @@ import android.app.Activity
 import android.app.ActivityManager
 import android.content.ComponentName
 import android.content.pm.PackageManager
+import android.content.res.ColorStateList
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.os.Build
+import android.os.Bundle
+import android.util.TypedValue
 import androidx.annotation.RequiresApi
 import com.jaredrummler.cyanea.Cyanea
+import com.jaredrummler.cyanea.R
 import com.jaredrummler.cyanea.utils.ColorUtils
+import com.jaredrummler.cyanea.utils.Reflection
 
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -19,6 +24,18 @@ internal open class CyaneaDelegateImplV21(
     private val cyanea: Cyanea,
     themeResId: Int)
   : CyaneaDelegateImplV19(activity, cyanea, themeResId) {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    if (cyanea.isThemeModified) {
+      when (Build.VERSION.SDK_INT) {
+        Build.VERSION_CODES.LOLLIPOP,
+        Build.VERSION_CODES.LOLLIPOP_MR1 -> {
+          preloadAccentColor()
+        }
+      }
+    }
+  }
 
   override fun onStart() {
     // Do not call super
@@ -52,6 +69,34 @@ internal open class CyaneaDelegateImplV21(
       }
     } catch (ignored: PackageManager.NameNotFoundException) {
     }
+  }
+
+  private fun preloadAccentColor() {
+    for (id in arrayOf(
+        R.color.color_accent
+    )) {
+      try {
+        val field = Reflection.getField(activity.resources, "sPreloadedColorStateLists") ?: return
+
+        val value = TypedValue()
+        val getValueMethod = Reflection.getMethod(activity.resources, "getValue",
+            Int::class.java, TypedValue::class.java, Boolean::class.java) ?: return
+        getValueMethod.invoke(activity.resources, id, value, true)
+        val key = value.assetCookie.toLong() shl 32 or value.data.toLong()
+
+        val csl = ColorStateList.valueOf(cyanea.accent)
+
+        field.get(null)?.let { cache ->
+          Reflection.getMethod(cache, "put", Long::class.java, Object::class.java)?.invoke(cache, key, csl)
+        }
+      } catch (ex: Throwable) {
+        Cyanea.log(TAG, "Error preloading colors", ex)
+      }
+    }
+  }
+
+  companion object {
+    private const val TAG = "CyaneaDelegateImplV21"
   }
 
 }
