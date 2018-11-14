d04cd40dda56e806c0ef725c144635f2292daca9
Jared Rummler <jared.rummler@gmail.com>
2018-11-13 22:21:03 -0800

    Safe call to preloadAccentColor

--- library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV24.kt
+++ library/src/main/java/com/jaredrummler/cyanea/delegate/CyaneaDelegateImplV24.kt
@@ -29,34 +29,42 @@ internal open class CyaneaDelegateImplV24(
 
   @SuppressLint("PrivateApi")
   private fun preloadAccentColor() {
-    val mResourcesImpl = Reflection.getFieldValue<Any?>(activity.resources, "mResourcesImpl") ?: return
+    try {
+      val mResourcesImpl = Reflection.getFieldValue<Any?>(activity.resources, "mResourcesImpl") ?: return
 
-    // Load the TypedValue and key
-    val value = TypedValue()
-    val getValueMethod = Reflection.getMethod(mResourcesImpl, "getValue",
-        Int::class.java, TypedValue::class.java, Boolean::class.java) ?: return
-    getValueMethod.invoke(mResourcesImpl, R.color.color_accent, value, true)
-    val key = value.assetCookie.toLong() shl 32 or value.data.toLong()
+      // Load the TypedValue and key
+      val value = TypedValue()
+      val getValueMethod = Reflection.getMethod(mResourcesImpl, "getValue",
+          Int::class.java, TypedValue::class.java, Boolean::class.java) ?: return
+      getValueMethod.invoke(mResourcesImpl, R.color.color_accent, value, true)
+      val key = value.assetCookie.toLong() shl 32 or value.data.toLong()
 
-    // Create a ColorStateList for the ColorStateListFactory
-    val color = Cyanea.getOriginalColor(R.color.color_accent)
-    val csl = ColorStateList.valueOf(color)
+      // Create a ColorStateList for the ColorStateListFactory
+      val color = Cyanea.getOriginalColor(R.color.color_accent)
+      val csl = ColorStateList.valueOf(color)
 
-    // Add our factory to ResourcesImpl.sPreloadedComplexColors
-    // This will use our factory instead of using ResourcesImpl#loadComplexColorForCookie
-    // This is needed so the correct accent color is cached
-    Class.forName("android.content.res.ColorStateList\$ColorStateListFactory").let { klass ->
-      val constructor = klass.getConstructor(ColorStateList::class.java) ?: return
-      if (!constructor.isAccessible) constructor.isAccessible = true
-      constructor.newInstance(csl)?.let { factory ->
-        Reflection.getField(mResourcesImpl, "sPreloadedComplexColors")?.let { field ->
-          field.get(null)?.let { factories ->
-            Reflection.getMethod(factories, "put", Long::class.java, Object::class.java)
-                ?.invoke(factories, key, factory)
+      // Add our factory to ResourcesImpl.sPreloadedComplexColors
+      // This will use our factory instead of using ResourcesImpl#loadComplexColorForCookie
+      // This is needed so the correct accent color is cached
+      Class.forName("android.content.res.ColorStateList\$ColorStateListFactory").let { klass ->
+        val constructor = klass.getConstructor(ColorStateList::class.java) ?: return
+        if (!constructor.isAccessible) constructor.isAccessible = true
+        constructor.newInstance(csl)?.let { factory ->
+          Reflection.getField(mResourcesImpl, "sPreloadedComplexColors")?.let { field ->
+            field.get(null)?.let { factories ->
+              Reflection.getMethod(factories, "put", Long::class.java, Object::class.java)
+                  ?.invoke(factories, key, factory)
+            }
           }
         }
       }
+    } catch (e: Exception) {
+      Cyanea.log(TAG, "Error preloading colors", e)
     }
+  }
+
+  companion object {
+    private const val TAG = "CyaneaDelegateImplV24"
   }
 
 }
