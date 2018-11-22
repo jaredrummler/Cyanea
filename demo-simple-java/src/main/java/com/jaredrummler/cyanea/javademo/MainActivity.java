e3b402a000616db00d7674ef04e0919dca91724a
Jared Rummler <jared.rummler@gmail.com>
2018-11-21 22:13:33 -0800

    Add a demo in Java

---
+++ demo-simple-java/src/main/java/com/jaredrummler/cyanea/javademo/MainActivity.java
@@ -0,0 +1,46 @@
+package com.jaredrummler.cyanea.javademo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.jaredrummler.cyanea.app.CyaneaAppCompatActivity;
+import com.jaredrummler.cyanea.prefs.CyaneaSettingsActivity;
+
+public class MainActivity extends CyaneaAppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    FloatingActionButton fab = findViewById(R.id.fab);
+    setSupportActionBar(toolbar);
+    fab.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View v) {
+        Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+            .setAction("Action", null).show();
+      }
+    });
+  }
+
+  @Override public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return super.onCreateOptionsMenu(menu);
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    switch (item.getItemId()) {
+      case R.id.action_settings:
+        startActivity(new Intent(this, CyaneaSettingsActivity.class));
+        return true;
+      default:
+        return super.onOptionsItemSelected(item);
+    }
+  }
+
+}
